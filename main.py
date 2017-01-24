9bb41f326c1753b47c483113dca688c0c3688fcb
Kevin De La Torre <kevindelatorre@live.com>
2017-01-24 11:41:59 -0800

    Started main program a bit, split pc/ir into separate files.

---
+++ main.py
@@ -0,0 +1,17 @@
+#   Author : Kevin De La Torre
+#   Purpose: Expand every line in assembly file and write to word file
+
+from sys import argv
+
+def main():
+    if ( len( argv ) > 1 ):
+
+    else:
+        print( "Usage: {0} <.asm>".format( argv[ 0 ] ) )
+
+def encoder( instruction ):
+
+
+
+ __name__ == "__main__":
+    main()
