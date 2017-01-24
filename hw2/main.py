6b042ab2767c36f34ce7213abd1ef2bf158056cb
Kevin De La Torre <kevindelatorre@live.com>
2017-01-24 15:48:48 -0800

    Output to file pretty much done, need to work on actual processing now.

--- hw2/main.py
+++ hw2/main.py
@@ -6,17 +6,59 @@ from docx import Document
 
 def main():
     if ( len( argv ) > 2 ):
-        pcFile = argv[ 1 ]
-        irFile = argv[ 2 ]
+        document = Document()
+        writeCoverPage( document )
+        pcFile = open( argv[ 1 ], 'r' )
+        irFile = open( argv[ 2 ], 'r' )
         
-        for ( pc in pcFile ):
-            irLine = irFile
+        for pc in pcFile:
+            irLine = irFile.readline()
+            attributes = decode( irLine )
+            #convertInstruction( irLine )
+            createTable( document, attributes )
+            document.add_page_break()
+
+        document.save( "CS365_Homework2.docx" )
     else:
         print( "Usage: {0} <pc> <ir>".format( argv[ 0 ] ) )
 
-def encoder( instruction ):
+def createTable( document, attributes ):
+    table = document.add_table( rows = 7, cols = 2 )
+    table.rows[ 0 ].cells[ 0 ].text = "PC ( Program Counter )"
+    table.rows[ 1 ].cells[ 0 ].text = "Instruction Register"
+    table.rows[ 2 ].cells[ 0 ].text = "PC ( Post-IR retrieval )"
+    table.rows[ 3 ].cells[ 0 ].text = "Instruction Type"
+    table.rows[ 4 ].cells[ 0 ].text = "Addressing Mode"
+    table.rows[ 5 ].cells[ 0 ].text = "MIPS Instruction"
+    table.rows[ 6 ].cells[ 0 ].text = "PC to be executed next"
+
+    table.rows[ 0 ].cells[ 1 ].text = "0x{:06X}".format( attributes[ 0 ] )
+    table.rows[ 1 ].cells[ 1 ].text = "0x{:08X}".format( attributes[ 1 ] )
+    table.rows[ 2 ].cells[ 1 ].text = "0x{:06X}".format( attributes[ 2 ] )
+    table.rows[ 3 ].cells[ 1 ].text = attributes[ 3 ]
+    table.rows[ 4 ].cells[ 1 ].text = attributes[ 4 ]
+    table.rows[ 5 ].cells[ 1 ].text = attributes[ 5 ]
+    table.rows[ 6 ].cells[ 1 ].text = "0x{:06X}".format( attributes[ 6 ] )
+
+def decode( instruction ):
+    pc = 0x400000
+    ir = 0x2012001E
+    pcPost = 0x4000004
+    type = "I-Type"
+    addressMode = "Immediate Addressing"
+    mipsInstruction = "addi $s2, $zero, 30"
+    pcToExec = 0x4000004
+    return ( pc, ir, pcPost, type, addressMode, mipsInstruction, pcToExec )
+
+
+def writeCoverPage( document ):
+    document.add_heading( "CS365 - Homework 2" )
+    document.add_paragraph( "Kevin De La Torre ( 010806250 )" )
+    document.add_page_break()
+
+#def encoder( instruction ):
 
 
 
- __name__ == "__main__":
+if __name__ == "__main__":
     main()
