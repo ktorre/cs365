9534c4561bd6e0c8b552c1b4eb269814abd5c566
Kevin De La Torre <kevindelatorre@live.com>
2017-01-25 11:32:13 -0800

    Bit of work into decoder

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,8 +1,8 @@
 #   Author : Kevin De La Torre
-#   Purpose: Expand every line in assembly file and write to word file
 
 from sys import argv
 from docx import Document
+import sys
 
 
 
@@ -10,8 +10,7 @@ def main():
     if ( len( argv ) > 2 ):
         instructionDict = loadInstructionDictionary()
         registerDict = loadRegisterDictionary()
-        printDictionary( instructionDict )
-        printDictionary( registerDict )
+        sys.stdout.write( "Processing..." )
         document = Document()
         writeCoverPage( document )
         pcFile = open( argv[ 1 ], 'r' )
@@ -19,34 +18,41 @@ def main():
         
         for pc in pcFile:
             irLine = irFile.readline()
-            attributes = decode( irLine )
-            #convertInstruction( irLine )
+            attributes = decode( irLine, instructionDict, registerDict, pc )
+            #convertInstruction( irLine, instructionDict, registerDict, pc )
             createTable( document, attributes )
             document.add_page_break()
 
+        print( "Done." )
         document.save( "CS365_Homework2.docx" )
     else:
         print( "Usage: {0} <pc> <ir>".format( argv[ 0 ] ) )
 
+'''
 def printDictionary( dict ):
     for key in dict.keys():
         print( "Key: {0}".format( key ) )
         print( "Value(s): {0}".format( dict.get( key ) ) )
+'''
 
 def loadRegisterDictionary():
+    sys.stdout.write( "Loading registers..." )
     file = open( "registers.dat", 'r' )
     registerDict = {}
     for line in file:
         line = line.split( ',', 2 )
         registerDict[ line[ 0 ] ] = line[ 1 ]
+    print( "Done." )
     return registerDict
 
 def loadInstructionDictionary():
+    sys.stdout.write( "Loading instructions..." )
     file = open( "instructions.dat", 'r' )
     instructionDict = {}
     for line in file:
         line = line.split( ',', 4 )
         instructionDict[ line[ 0 ] ] = line[ 1:4 ]
+    print( "Done." )
 
     return instructionDict
 
@@ -71,25 +77,33 @@ def createTable( document, attributes ):
     table.rows[ 5 ].cells[ 1 ].text = attributes[ 5 ]
     table.rows[ 6 ].cells[ 1 ].text = "0x{:06X}".format( attributes[ 6 ] )
 
-def decode( instruction ):
-    pc = 0x400000
+#def convertInstruction( currentInstruction ):
+    
+
+def decode( instruction, instructions, registers, pc ):
+    pc = int( pc, 16 )
+
+    instructionName = instruction.split( ' ' )[ 0 ]
+    instruction
+    instructionValues = instructions.get( instructionName )
+    print( "Name: ", instructionName )
+    print( "Values: ", instructionValues )
+    print()
+    
     ir = 0x2012001E
     pcPost = 0x4000004
     type = "I-Type"
     addressMode = "Immediate Addressing"
     mipsInstruction = "addi $s2, $zero, 30"
     pcToExec = 0x4000004
-    return ( pc, ir, pcPost, type, addressMode, mipsInstruction, pcToExec )
+    hex = 0x00000000
+    return ( pc, ir, pcPost, type, addressMode, mipsInstruction, pcToExec, hex )
 
 
 def writeCoverPage( document ):
     document.add_heading( "CS365 - Homework 2" )
     document.add_paragraph( "Kevin De La Torre ( 010806250 )" )
     document.add_page_break()
-
-#def encoder( instruction ):
-
-
 
 if __name__ == "__main__":
     main()
